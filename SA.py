ea6b5a19f1425d2e8850c504473a923e7b6394d8
AbhirupChakraborty <59620162+AbhirupChakraborty@users.noreply.github.com>
2020-07-31 10:37:29 +0530

    Update SA.py

--- SA.py
+++ SA.py
@@ -1,16 +1,37 @@
 import string
 from collections import Counter
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import matplotlib.pyplot as plt
-text=open('read.txt',encoding='utf-8').read()
+import GetOldTweets3  as got
+from datetime import date
+def get_tweets():
+    today_date=date.today()
+    tweetCriteria = got.manager.TweetCriteria().setQuerySearch('Internship') \
+        .setSince("2020-01-01") \
+        .setUntil(str(today_date)) \
+        .setMaxTweets(100)
+    tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+    text_tweets=[[tweet.text] for tweet in tweets]
+    return text_tweets
+text_tweet=get_tweets()
+text=""
+for i in text_tweet:
+    text=i[0]+" "+text
 lower_case=text.lower()
 cleaned_text=lower_case.translate(str.maketrans('','',string.punctuation))
-tokenization_word=word_tokenize(cleaned_text,"english")
+tokenization_word=cleaned_text.split()
+stop_words=["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 final_words=[]
 for i in tokenization_word:
-    if i not in stopwords.words('english'):
+    if i not in stop_words:
         final_words.append(i)
 emotion_list=[]
 with open('emotions.txt','r') as f:
@@ -21,20 +42,6 @@ with open('emotions.txt','r') as f:
             emotion_list.append(emotion)
 # print(emotion_list)
 w=Counter(emotion_list)
-# print(w)
-
-def sentiment_analyse(sentiment_text):
-    score=SentimentIntensityAnalyzer().polarity_scores(sentiment_text)
-    neg=score['neg']
-    pos=score['pos']
-    if neg>pos:
-        print("Negative Sentiment")
-    elif pos>neg:
-        print("Positive Sentiment")
-    else:
-        print("Nutral Sentiment")
-sentiment_analyse(cleaned_text)
-
 fig,axl=plt.subplots()
 axl.bar(w.keys(),w.values())
 fig.autofmt_xdate()
